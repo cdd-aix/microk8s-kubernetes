bdb6171d84892387e3f0b5073fbaa3c9ab1d49d0
Chris Dukes <chris.dukes.aix@gmail.com>
2021-03-27 10:42:51 -0400

    Add the actual install

---
+++ install-calico-and-join-windows-worker-node.ps1
@@ -0,0 +1,9 @@
+$ErrorActionPreference = "Stop"
+Write-Output "*** Installing calico services"
+set-psdebug -Trace 2
+c:\CalicoWindows\install-calico.ps1
+Write-Output "*** Install Kubernetes services"
+c:\CalicoWindows\kubernetes\install-kube-services.ps1
+Write-Output "*** Starting kubelet and kube-proxy"
+Start-Service kubelet
+Start-Service kube-proxy
