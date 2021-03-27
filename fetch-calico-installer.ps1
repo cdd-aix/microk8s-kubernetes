04bcc1b6899d8398754bd4fa41cd394316981ef1
Chris Dukes <chris.dukes.aix@gmail.com>
2021-03-27 10:42:29 -0400

    Separate the fetch from the install and name better

--- fetch-calico-installer.ps1
+++ fetch-calico-installer.ps1
@@ -1,12 +1,11 @@
 $ErrorActionPreference = "Stop"
 mkdir -force c:\k
 copy -force c:\vagrant\.kube\config c:\k\config
+cd \
+Write-Output "*** Downloading calico installer"
 Invoke-WebRequest https://docs.projectcalico.org/scripts/install-calico-windows.ps1 -OutFile c:\install-calico-windows.ps1
 $kubeRawVersion = get-content "C:\vagrant\.kube\version"
 $kubeVersion = $kubeRawVersion -replace "`n","" -replace "`r",""
 # Note we are missing calico-system namespace
+Write-Output "*** Using calico installer to install Kubernetes " $kubeVersion
 c:\install-calico-windows.ps1 -DownloadOnly yes -KubeVersion $kubeVersion
-c:\CalicoWindows\install-calico.ps1
-c:\CalicoWindows\kubernetes\install-kube-services.ps1
-Start-Service kubelet
-Start-Service kube-proxy
